4886a612a118bec7ab7ad0b09e17db4348a9aa37
Michael Mo <michaelmoshui@gmail.com>
2022-12-28 16:46:11 -0500

    Change port

--- app.js
+++ app.js
@@ -137,6 +137,9 @@ app.post("/submit", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, function() {
   console.log("Server is up and running.");
 });
